1066e1b19ce0ae6fbc82a1f14eda64cdfb625dbb
user <user@MacBook-Air-sl-user.local>
2025-12-03 17:40:56 +0200

    install husky

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,29 +1,17 @@
 import express from 'express';
-import type { Request,Response } from 'express';
+import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
-app.get('/', (req: Request,res:Response)=> {
-    res.send('hello world!');
+app.get('/', (req: Request, res: Response) => {
+   res.send('hello world!');
 });
-app.get('/api/hello', (req: Request,res:Response)=> {
-    res.json({message:'hello world!'});
+app.get('/api/hello', (req: Request, res: Response) => {
+   res.json({ message: 'hello world!' });
 });
 
 app.listen(port, () => {
-    console.log(`server is running on http://localhost:${port}`)
+   console.log(`server is running on http://localhost:${port}`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
