35c0dfb19959f2fdd058e84c09a8d6a131d33496
shaked_arazi <shaked13579@gmail.com>
2026-01-04 21:46:00 +0200

    frontend

--- packages/client/src/components/chat/ChatMessages.tsx
+++ packages/client/src/components/chat/ChatMessages.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from 'react';
-import ReactMarkDown from 'react-markdown';
+import { useEffect, useRef } from 'react';
+import ReactMarkdown from 'react-markdown';
 
 export type Message = {
    content: string;
@@ -11,10 +11,12 @@ type Props = {
 };
 
 const ChatMessages = ({ messages }: Props) => {
-   const lastMessageref = useRef<HTMLDivElement | null>(null);
+   const lastMessageRef = useRef<HTMLDivElement | null>(null);
+
    useEffect(() => {
-      lastMessageref.current?.scrollIntoView({ behavior: 'smooth' });
+      lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
    }, [messages]);
+
    const onCopyMessage = (e: React.ClipboardEvent) => {
       const selection = window.getSelection()?.toString().trim();
       if (selection) {
@@ -22,26 +24,23 @@ const ChatMessages = ({ messages }: Props) => {
          e.clipboardData.setData('text/plain', selection);
       }
    };
+
    return (
-      <div>
-         {messages.map((message, index) => {
-            return (
-               <div className="flex flex-col gap-3 mb-10 flex items-center">
-                  <div
-                     key={index}
-                     onCopy={onCopyMessage}
-                     ref={index === messages.length - 1 ? lastMessageref : null}
-                     className={`px-3 py-1 max-w-md rounded-xl ${
-                        message.role === 'user'
-                           ? ' text-black self-end bg-gradient-to-br from-purple-700 to-blue-900 text-white'
-                           : ' text-black self-start bg-gray-100'
-                     }`}
-                  >
-                     {<ReactMarkDown>{message.content}</ReactMarkDown>}
-                  </div>
-               </div>
-            );
-         })}
+      <div className="flex flex-col gap-3">
+         {messages.map((message, index) => (
+            <div
+               key={index}
+               onCopy={onCopyMessage}
+               ref={index === messages.length - 1 ? lastMessageRef : null}
+               className={`px-3 py-1 max-w-md rounded-xl prose ${
+                  message.role === 'user'
+                     ? 'bg-blue-600 text-white self-end'
+                     : 'bg-gray-100 text-black self-start'
+               }`}
+            >
+               <ReactMarkdown>{message.content}</ReactMarkdown>
+            </div>
+         ))}
       </div>
    );
 };
