c8d0a1760e5f746466b638ac434078e702a3aa5b
shaked_arazi <shaked13579@gmail.com>
2026-01-06 20:22:59 +0200

    controllers

--- packages/server/controllers/review.controller.ts
+++ packages/server/controllers/review.controller.ts
@@ -1,20 +1,20 @@
 import type { Request, Response } from 'express';
+import { reviewService } from '../services/review.service';
 import { productRepository } from '../repositories/product.repository';
 import { reviewRepository } from '../repositories/review.repository';
-import { reviewService } from '../services/review.service';
 
 export const reviewController = {
    async getReviews(req: Request, res: Response) {
       const productId = Number(req.params.id);
 
       if (isNaN(productId)) {
-         res.status(400).json({ error: 'Invalid product ID.' });
+         res.status(400).json({ error: 'Invaild product ID.' });
          return;
       }
 
       const product = await productRepository.getProduct(productId);
       if (!product) {
-         res.status(404).json({ error: 'Product does not exist.' });
+         res.status(400).json({ error: 'Product does not exist.' });
          return;
       }
 
@@ -31,7 +31,7 @@ export const reviewController = {
       const productId = Number(req.params.id);
 
       if (isNaN(productId)) {
-         res.status(400).json({ error: 'Invalid product ID.' });
+         res.status(400).json({ error: 'Invaild product ID.' });
          return;
       }
 
@@ -43,7 +43,7 @@ export const reviewController = {
 
       const reviews = await reviewRepository.getReviews(productId, 1);
       if (!reviews.length) {
-         res.status(400).json({ error: 'There are no reviews to summarize.' });
+         res.status(400).json({ error: 'There are no reviews to Summarize.' });
          return;
       }
 
