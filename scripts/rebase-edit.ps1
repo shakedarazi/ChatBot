f38e7af7af1876fc99d69918006ab6ff2ac8ad97
shaked_arazi <shaked13579@gmail.com>
2026-02-13 16:43:51 +0200

    only readme upply

--- scripts/rebase-edit.ps1
+++
@@ -1,3 +0,0 @@
-$path = $args[1]
-if (-not $path) { $path = $args[0] }
-(Get-Content $path) -replace '^pick ', 'edit ' | Set-Content $path
--- scripts/strip-coauthor.ps1
+++
@@ -1,3 +0,0 @@
-# Strip Co-authored-by: use only the subject line (first line) as the new message
-$subject = (git log -1 --pretty=%s)
-git -c user.name="shaked_arazi" -c user.email="shaked13579@gmail.com" commit --amend --no-verify -m $subject --reset-author
